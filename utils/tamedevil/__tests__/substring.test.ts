1641742ca48a5aabfdf4b1875513db10b1d2fa99
Benjie Gillam <benjie@jemjie.com>
2026-05-04 20:33:55 +0100

    More thorough tests

--- utils/tamedevil/__tests__/substring.test.ts
+++ utils/tamedevil/__tests__/substring.test.ts
@@ -1,10 +1,34 @@
 import type { TE } from "../dist/index.js";
 import te from "../dist/index.js";
 
-const evilString = "'\"` \\'\\\"\\` ${foo}\\${foo}\\\\${foo}";
-const simpleString = "aaa${a}aaa";
+// Make the string longer so it goes through the non-optimized path
+const long = "a".repeat(2000);
 
-const testCases: Array<[TE, string]> = [
+const testStrings = [
+  "",
+  "a",
+  "abcdefghijklmnopqrstuvwxyz",
+  "\\",
+  "'",
+  "\\'",
+  "\\\\'",
+  '"',
+  '\\"',
+  '\\\\"',
+  "`",
+  "\\`",
+  "\\\\`",
+  "aaa${a}aaa",
+  "aaa\\${a}aaa",
+  "aaa\\\\${a}aaa",
+  "aaa$${a}aaa",
+  "aaa$\\${a}aaa",
+  "aaa\\$${a}aaa",
+  "aaa\\$\\${a}aaa",
+  "'\"` \\'\\\"\\` ${foo}\\${foo}\\\\${foo}",
+];
+
+const testCases: Array<[TE, string]> = testStrings.flatMap((evilString) => [
   [
     te`const foo = 7; return "abc${te.substring(evilString, '"')}123"`,
     `abc` + evilString + `123`,
@@ -18,18 +42,18 @@ const testCases: Array<[TE, string]> = [
     `abc` + evilString + `123`,
   ],
   [
-    te`const foo = 7; return "abc${te.substring(simpleString, '"')}123"`,
-    `abc` + simpleString + `123`,
+    te`const foo = 7; return "abc${te.substring(long + evilString, '"')}123"`,
+    `abc` + long + evilString + `123`,
   ],
   [
-    te`const foo = 7; return 'abc${te.substring(simpleString, "'")}123'`,
-    `abc` + simpleString + `123`,
+    te`const foo = 7; return 'abc${te.substring(long + evilString, "'")}123'`,
+    `abc` + long + evilString + `123`,
   ],
   [
-    te`const foo = 7; return \`abc${te.substring(simpleString, "`")}123\``,
-    `abc` + simpleString + `123`,
+    te`const foo = 7; return \`abc${te.substring(long + evilString, "`")}123\``,
+    `abc` + long + evilString + `123`,
   ],
-];
+]);
 
 it.each(testCases)("foo", (teNode, expected) => {
   const compiled = te.compile(teNode);
