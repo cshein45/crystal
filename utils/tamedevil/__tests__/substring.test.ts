0baceb702a6e284adcb0dda76896e8171994c231
Benjie Gillam <benjie@jemjie.com>
2026-05-04 20:23:57 +0100

    Fix escaping of dollar

--- utils/tamedevil/__tests__/substring.test.ts
+++ utils/tamedevil/__tests__/substring.test.ts
@@ -2,6 +2,7 @@ import type { TE } from "../dist/index.js";
 import te from "../dist/index.js";
 
 const evilString = "'\"` \\'\\\"\\` ${foo}\\${foo}\\\\${foo}";
+const simpleString = "aaa${a}aaa";
 
 const testCases: Array<[TE, string]> = [
   [
@@ -15,6 +16,18 @@ const testCases: Array<[TE, string]> = [
   [
     te`const foo = 7; return \`abc${te.substring(evilString, "`")}123\``,
     `abc` + evilString + `123`,
+  ],
+  [
+    te`const foo = 7; return "abc${te.substring(simpleString, '"')}123"`,
+    `abc` + simpleString + `123`,
+  ],
+  [
+    te`const foo = 7; return 'abc${te.substring(simpleString, "'")}123'`,
+    `abc` + simpleString + `123`,
+  ],
+  [
+    te`const foo = 7; return \`abc${te.substring(simpleString, "`")}123\``,
+    `abc` + simpleString + `123`,
   ],
 ];
 
